0198ba93b1abdb9368304ddcd57a677ab1429f67
Edward Tanguay (hm.win) <edward@tanguay.info>
2020-09-25 22:47:28 +0200

    Make flashcard in file work

--- src/app.js
+++ src/app.js
@@ -1,9 +1,27 @@
+
+const flashcardComponent = {
+	data: function () {
+		return {
+			front: 'house',
+			back: 'Haus'
+		}
+	},
+	template: '<div><div>{{front}} / {{back}}</div></div>'
+};
+
 new Vue({
 	el: "#app",
+	components: {
+		'flashcard-component': flashcardComponent
+	},
 	data: {
 		message: "hello"
 	},
 	template: `
-		<div>The message is: {{message}}</div>
+		<div>
+			<div>The message is: {{message}}</div>
+			<div><flashcard-component></flashcard-component></div>
+			<div><flashcard-component></flashcard-component></div>
+		</div>
 	`
 });
